3f72809f39f64828ddfc825ce862037cd9394147
Thanuj <ranjeewa_tj@yahoo.com>
2017-01-23 01:01:16 -0500

    intial commit with mongojs angularjs and expressset up with get and post

---
+++ public/Controllers/controller.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+
+    $http({
+        method: 'GET',
+        url: '/contactlist'
+    })
+        .then(function(response) {
+            console.log("recived the data");
+            $scope.contactlist =  response.data;
+        });
+
+    $scope.addContact = function () {
+        console.log($scope.contact);
+
+        $http.post('/contactlist', $scope.contact).then(function (response) {
+            console.log(response);
+        });
+        
+    }
+
+}]);
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+
+app.get('/contactlist',function (req,res) {
+    console.log("I recived the get request")
+
+    db.contactlist.find(function (err,docs) {
+        console.log(docs);
+        res.json(docs)
+    })
+
+
+});
+
+app.post('/contactlist',function (req,res) {
+    console.log("posting data");
+    console.log(req.body);
+    db.contactlist.insert(req.body,function (err,doc) {
+        res.json(doc);
+    })
+});
+
+app.listen(3000);
+console.log("server is running on port 3000 TJ")
+
